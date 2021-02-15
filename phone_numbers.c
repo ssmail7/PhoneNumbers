1951d2fbdb4507b229bb8ffbeca11ee9db784041
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 12:02:28 -0500

    Update phone_numbers.c

--- phone_numbers.c
+++ phone_numbers.c
@@ -1,4 +1,4 @@
-//**********************************************************************
+//*********************************************************************
 // Written by Steven Smail for COP3514
 //
 // phone_numbers.c (Project 7)
@@ -6,7 +6,7 @@
 // This program reads the content of a file and translates seven-letter
 // words into their corresponding phone numbers, then writes the phone
 // numbers to a file with the same name but an added extension of .cvt.
-//**********************************************************************
+//*********************************************************************
 
 #include <stdio.h>
 #include <string.h>
@@ -28,7 +28,14 @@ int main(void)
   scanf("%s", file1);
 
 //Opening the file to be read
-  FILE *pFile;  pFile = fopen(file1, "r");  if (pFile == NULL)  {    printf("File cannot be found\n");    return 1;  }
+  FILE *pFile;
+  pFile = fopen(file1, "r");
+
+  if (pFile == NULL)
+  {
+    printf("File cannot be found\n");
+    return 1;
+  }
 
 //Naming the ".cvt" file
   strcpy(file2, file1);
@@ -38,8 +45,11 @@ int main(void)
   FILE *qFile;
   qFile = fopen(file2, "w");
 
-  while (fgets(str1, WORD_LEN, pFile) != NULL)  {
-    translate(str1, str2);    fputs(str2, qFile);  }
+  while (fgets(str1, WORD_LEN, pFile) != NULL)
+  {
+    translate(str1, str2);
+    fputs(str2, qFile);
+  }
 
 //Closing file to be written
   fclose(qFile);
